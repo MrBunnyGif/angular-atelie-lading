825130c160c172f3f9e92529a65ca351865ec1a2
pedroBotega99 <62257171+pedroBotega99@users.noreply.github.com>
2021-01-11 15:13:23 -0300

    scroll melhorado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'atelie-landing-page';
+  @ViewChild('forms', { static: true }) forms: ElementRef;
+
+  ngOnInit(): void { }
+
+  scroll() {
+    this.forms.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
 }
