404d9897e1cbde732d316b220ab6955197536738
pedroBotega99 <62257171+pedroBotega99@users.noreply.github.com>
2021-01-11 12:15:12 -0300

    cadastrar: verificando cpf email e nome

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,86 +1,88 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
-  form: FormGroup;
-  hidePassword: boolean = true;
-  validationMessages = {
-    nome: {
-      required: 'Insira um nome',
-    },
-    cpf: {
-      required: 'Insira um valor de CPF',
-      error: 'Valor inserido não válido',
-    },
-    senha: {
-      required: 'Insira uma senha',
-      minlenght: 'Mínimo de 6 caractéres',
-      notSame: 'As senhas não conferem',
-    },
-    email: {
-      required: 'Insira um endereço de e-mail',
-      error: 'Endereço de e-mail não válido',
+  @ViewChild('name') primeiroCampo: ElementRef;
+  @ViewChild('mail') mail: ElementRef;
+  senha = "";
+  confirmSenha = "";
+  empresa = "";
+  classificacao = "";
+  hidePassword: boolean = true
+  errors = {
+    email: false
+  }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  nome = new FormControl('', [Validators.required]);
+  // CPF = new FormControl('', [Validators.required]);
+  CPF = new FormControl({ value: null, disabled: false }, this.isValidCpf());
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a valInsira um endereço de e-mailue';
     }
-  }
-  @ViewChild('nome') primeiroCampo: ElementRef;
-  formErrors = {
-    name: '',
-    email: '',
-    cpf: '',
-    senha: '',
+    return this.email.hasError('email') ? 'O e-mail não é válido' : '';
   }
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.setForms();
-  }
+  ngOnInit(): void { }
 
   ngAfterViewInit(): void {
     setTimeout(() => this.primeiroCampo.nativeElement.focus(), 300);
   }
 
-  setForms() {
-    this.form = this.formBuilder.group({
-      name: ['', [
-        Validators.required
-      ]],
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      cpf: ['', [
-        Validators.required,
-      ]],
-      empresa: [''],
-      classificacao: [''],
-      senha: ['', [
-        Validators.required,
-        Validators.minLength(6)
-      ]],
-      confirmSenha: ['', [
-        Validators.required,
-        Validators.minLength(6)
-      ]],
-    });
-  }
+  isValidCpf() {
+    return (control: AbstractControl): Validators => {
+      const cpf = control.value;
+      if (cpf) {
+        let numbers, digits, sum, i, result, equalDigits;
+        equalDigits = 1;
+        if (cpf.length < 11) {
+          return null;
+        }
 
-  register() {
-    this.verifyPassword();
-    if (this.form.invalid)
-      return
-    alert('cadastrado com sucesso')
-  }
+        for (i = 0; i < cpf.length - 1; i++) {
+          if (cpf.charAt(i) !== cpf.charAt(i + 1)) {
+            equalDigits = 0;
+            break;
+          }
+        }
 
-  verifyPassword() {
-    if (this.form.value.senha != this.form.value.confirmSenha)
-      this.formErrors.senha = this.validationMessages.senha.notSame;
-      console.log('formErrors.senha: ', this.formErrors.senha)
+        if (!equalDigits) {
+          numbers = cpf.substring(0, 9);
+          digits = cpf.substring(9);
+          sum = 0;
+          for (i = 10; i > 1; i--) {
+            sum += numbers.charAt(10 - i) * i;
+          }
+
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(0))) {
+            return { cpfNotValid: true };
+          }
+          numbers = cpf.substring(0, 10);
+          sum = 0;
+
+          for (i = 11; i > 1; i--) {
+            sum += numbers.charAt(11 - i) * i;
+          }
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(1))) {
+            return { cpfNotValid: true };
+          }
+          return null;
+        } else {
+          return { cpfNotValid: true };
+        }
+      }
+      return null;
+    };
   }
 }
